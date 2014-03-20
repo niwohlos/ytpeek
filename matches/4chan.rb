0fd5dca1c76d895a0f132261f3427a0f6be1f00b
Max Reitz <xanclic@gmail.com>
2014-03-20 19:34:28 +0100

    4chan: Implement spoiler tag; don't stop on <br>
    Implement the spoiler tag for text (not images) by replacing <\/?s> by
    \023 (IRC strikethrough).
    
    Also, since the bot now converts <br> to ⁋ and shortens the title as
    necessary, there is no longer a need to stop output on <br>.

--- matches/4chan.rb
+++ matches/4chan.rb
@@ -10,7 +10,7 @@
         title:
         {
             pattern: [ /.*/m ],
-            group: 'post = JSON.parse(matches[0][0])["posts"].select { |p| p["no"].to_i == um[3].to_i }[0]; if post; content = "[#{Time.at(post["time"].to_i).strftime("%T")}] #{unless post["com"].nil?; post["com"].gsub(/<br.*$/, " […]"); else; ""; end}"; if post["tim"]; content + " (http://images.4chan.org/#{um[2]}/src/#{post["tim"]}#{post["ext"]})"; else; content; end; else "[Post im Thread nicht gefunden]"; end',
+            group: 'post = JSON.parse(matches[0][0])["posts"].select { |p| p["no"].to_i == um[3].to_i }[0]; if post; content = "[#{Time.at(post["time"].to_i).strftime("%T")}] #{unless post["com"].nil?; post["com"].gsub(/<\/?s>/, "\023"); else; ""; end}"; if post["tim"]; content + " (http://images.4chan.org/#{um[2]}/src/#{post["tim"]}#{post["ext"]})"; else; content; end; else "[Post im Thread nicht gefunden]"; end',
         },
         cleanurl: "http://boards.4chan.org/<match>.json",
         remove:
@@ -28,7 +28,7 @@
         title:
         {
             pattern: [ /.*/m ],
-            group: 'op = JSON.parse(matches[0][0])["posts"][0]; "[#{Time.at(op["time"].to_i).strftime("%T")}] #{unless op["sub"].nil? || op["sub"].empty?; op["sub"]; else; unless op["com"].nil?; op["com"].gsub(/<br.*$/, " […]"); else; ""; end; end} (http://images.4chan.org/#{um[2]}/src/#{op["tim"]}#{op["ext"]})"',
+            group: 'op = JSON.parse(matches[0][0])["posts"][0]; "[#{Time.at(op["time"].to_i).strftime("%T")}] #{unless op["sub"].nil? || op["sub"].empty?; op["sub"]; else; unless op["com"].nil?; op["com"].gsub(/<\/?s>/, "\023"); else; ""; end; end} (http://images.4chan.org/#{um[2]}/src/#{op["tim"]}#{op["ext"]})"',
         },
         cleanurl: "http://boards.4chan.org/<match>.json",
         remove:
