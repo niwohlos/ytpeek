690881b6050a478a57ea2718b8d7b94a573cc73c
Max Reitz <xanclic@niwohlos.org>
2015-10-03 19:00:54 +0200

    Fix Twitter (no timestamp, though)

--- matches/twitter.rb
+++ matches/twitter.rb
@@ -9,8 +9,8 @@
         ],
         title:
         {
-            pattern: [ /<p\s+class="[^"]*tweet-text[^"]*">(([^<]*|<[^\/]|<\/[^p])*)<\/p>/, /<span\s+class="[^"]*_timestamp[^"]*"\s+data-time="(\d+)"/, /<span>&rlm;<\/span><span\s+class="[^"]*username[^"]*"[^>]*><s>([^<]*)<\/s><b>([^<]*)</ ],
-            group: '"[#{if Time.at(matches[1][1].to_i).strftime(\'%F\') == Time.new.strftime(\'%F\'); Time.at(matches[1][1].to_i).strftime(\'%T\'); else; Time.at(matches[1][1].to_i).strftime(\'%A, %-d. %B %Y] [%T\'); end}] &lt;#{matches[2][1]}#{matches[2][2]}&gt; #{matches[0][1]}"',
+            pattern: [ /<meta\s+property="og:description"\s+content="“?([^”"]*)/, /<strong\s+class="[^"]*fullname[^"]*"[^>]*>([^<]*)/ ],
+            group: '"&lt;#{matches[1][1]}&gt; #{matches[0][1]}"'
         },
         cleanurl: "https://twitter.com/",
         remove:
