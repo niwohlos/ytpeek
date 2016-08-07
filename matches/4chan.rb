27515bfa2b555e627cda464879b2207833f1e4be
Max Reitz <xanclic@niwohlos.org>
2016-08-07 13:21:04 +0200

    Fix 4chan timestamp

--- matches/4chan.rb
+++ matches/4chan.rb
@@ -1,5 +1,3 @@
-require 'tzinfo'
-
 [
     {
         resources:
@@ -12,7 +10,7 @@ require 'tzinfo'
         title:
         {
             pattern: [ /.*/m ],
-            group: 'post = JSON.parse(matches[0][0])["posts"].find { |p| p["no"].to_i == um[4].to_i }; if post; content = "[#{TZInfo::Timezone.get("Europe/Berlin").utc_to_local(Time.at(post["time"].to_i)).strftime("%T")}] #{unless post["com"].nil?; post["com"].gsub(/<\/?s>/, "\023"); else; ""; end}"; if post["tim"]; content + " (http://images.4chan.org/#{um[2]}/src/#{post["tim"]}#{post["ext"]})"; else; content; end; else "[Post im Thread nicht gefunden]"; end',
+            group: 'post = JSON.parse(matches[0][0])["posts"].find { |p| p["no"].to_i == um[4].to_i }; if post; content = "[#{Time.at(post["time"].to_i).strftime("%T")}] #{unless post["com"].nil?; post["com"].gsub(/<\/?s>/, "\023"); else; ""; end}"; if post["tim"]; content + " (http://images.4chan.org/#{um[2]}/src/#{post["tim"]}#{post["ext"]})"; else; content; end; else "[Post im Thread nicht gefunden]"; end',
         },
         cleanurl: "http://a.4cdn.org/<match>.json",
         remove:
@@ -30,7 +28,7 @@ require 'tzinfo'
         title:
         {
             pattern: [ /.*/m ],
-            group: 'op = JSON.parse(matches[0][0])["posts"][0]; "[#{TZInfo::Timezone.get("Europe/Berlin").utc_to_local(Time.at(op["time"].to_i)).strftime("%T")}] #{unless op["sub"].nil? || op["sub"].empty?; op["sub"]; else; unless op["com"].nil?; op["com"].gsub(/<\/?s>/, "\023"); else; ""; end; end} (http://images.4chan.org/#{um[2]}/src/#{op["tim"]}#{op["ext"]})"',
+            group: 'op = JSON.parse(matches[0][0])["posts"][0]; "[#{Time.at(op["time"].to_i).strftime("%T")}] #{unless op["sub"].nil? || op["sub"].empty?; op["sub"]; else; unless op["com"].nil?; op["com"].gsub(/<\/?s>/, "\023"); else; ""; end; end} (http://images.4chan.org/#{um[2]}/src/#{op["tim"]}#{op["ext"]})"',
         },
         cleanurl: "http://a.4cdn.org/<match>.json",
         remove:
